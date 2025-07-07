4a116151e31bdcb41a49878edeed81013dd1a838
Vinayak Gupta <guptavinayak1614@gmail.com>
2025-07-07 09:15:05 +0530

    Update SuperAdminDashboard.jsx

--- frontend/src/pages/SuperAdminDashboard.jsx
+++ frontend/src/pages/SuperAdminDashboard.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import UserForm from '../components/UserForm';
 import SidebarLayout from '../components/SidebarLayout';
-import '../css/superAdminDashboard.css';
+import '../CSS/superAdminDashboard.css';
 
 const SuperAdminDashboard = () => {
   const [users, setUsers] = useState([]);
